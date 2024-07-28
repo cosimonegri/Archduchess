6393a71383fdd31ba5fe50449b417f166f084116
cosimonegri <cosimogiovanninegri@gmail.com>
2024-07-28 14:13:43 +0200

    Optimize move constexpr

--- src/move.hpp
+++ src/move.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <cstdint>
+#include <cassert>
 #include "types.hpp"
 
 namespace engine
@@ -25,25 +26,57 @@ namespace engine
         QUEEN_PROM_CAPTURE,
     };
 
+    constexpr int captureMask = CAPTURE << 12;
+    constexpr int promotionMask = KNIGHT_PROM << 12;
+
     class Move
     {
     private:
         uint16_t move;
 
     public:
-        Move();
-        Move(Tile from, Tile to, MoveFlag flag = QUIET);
+        constexpr Move()
+            : move(0) {};
+        constexpr Move(Tile from, Tile to, MoveFlag flag = QUIET)
+            : move(flag << 12 | to << 6 | from) {};
 
-        Tile getFrom() const;
-        Tile getTo() const;
-        MoveFlag getFlag() const;
+        constexpr Tile getFrom() const
+        {
+            assert(isValid());
+            return Tile(move & 0x3f);
+        }
+        constexpr Tile getTo() const
+        {
+            assert(isValid());
+            return Tile((move >> 6) & 0x3f);
+        }
 
-        bool isCapture();
-        bool isPromotion();
-        bool isCastling();
+        constexpr MoveFlag getFlag() const
+        {
+            assert(isValid());
+            return MoveFlag((move >> 12) & 0x0f);
+        }
 
-        bool operator==(Move other) const { return move == other.move; }
-        bool operator!=(Move other) const { return move != other.move; }
+        constexpr bool isCapture() const
+        {
+            return (move & captureMask) != 0;
+        }
+        constexpr bool isPromotion() const
+        {
+            return (move & promotionMask) != 0;
+        }
+        constexpr bool isCastling() const
+        {
+            return (getFlag() == KING_CASTLE || getFlag() == QUEEN_CASTLE);
+        }
+
+        constexpr bool isValid() const
+        {
+            return move != 0;
+        }
+
+        constexpr bool operator==(Move other) const { return move == other.move; }
+        constexpr bool operator!=(Move other) const { return move != other.move; }
 
         friend std::ostream &operator<<(std::ostream &stream, const engine::Move &move)
         {
@@ -51,9 +84,6 @@ namespace engine
             return stream;
         };
     };
-
-    constexpr int captureMask = CAPTURE << 12;
-    constexpr int promotionMask = KNIGHT_PROM << 12;
 }
 
 #endif

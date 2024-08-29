af18021676d9a4b0b5099e1b7ec7b9132bfec250
cosimonegri <cosimogiovanninegri@gmail.com>
2024-08-29 14:46:32 +0200

    Test mate positions

--- src/tests/test.cpp
+++ src/tests/test.cpp
@@ -1,6 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include <vector>
 #include <tuple>
+#include "bot.hpp"
 #include "perft.hpp"
 #include "position.hpp"
 #include "zobrist.hpp"
@@ -11,7 +12,7 @@ TEST_CASE("PerftTest", "[engine]")
     engine::bitboard::init();
     engine::zobrist::init();
 
-    std::vector<std::tuple<std::string, int, uint64_t>> perftTestCases = {
+    std::vector<std::tuple<std::string, int, uint64_t>> testCases = {
         std::make_tuple("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", 4, 197281),
         std::make_tuple("r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1", 4, 4085603),
         std::make_tuple("8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1", 4, 43238),
@@ -43,7 +44,7 @@ TEST_CASE("PerftTest", "[engine]")
         std::make_tuple("8/8/2k5/5q2/5n2/8/5K2/8 b - - 0 1", 4, 23527),
     };
 
-    for (const auto &testCase : perftTestCases)
+    for (const auto &testCase : testCases)
     {
         std::string fen;
         int depth;
@@ -53,5 +54,33 @@ TEST_CASE("PerftTest", "[engine]")
         engine::Position pos(fen);
         uint64_t actual = engine::perft(pos, depth, false);
         REQUIRE(actual == expected);
+    }
+}
+
+TEST_CASE("BotTest", "[engine]")
+{
+    engine::bitboard::init();
+    engine::zobrist::init();
+
+    std::vector<std::tuple<std::string, std::string>> testCases = {
+        // mate in 1
+        std::make_tuple("5r1k/6pp/p1P5/1p1QB2n/3P4/P4pPq/7P/5RK1 b - - 1 31", "h3g2"),
+        std::make_tuple("Q4nk1/p1p2pp1/7p/3P4/2BN1Rb1/2N1P1n1/PP3KP1/q7 b - - 0 21", "g3h1"),
+        std::make_tuple("5r2/p1p3R1/2pk4/2Np3p/3Pp3/2P5/PP3rP1/6K1 w - - 0 39", "g7d7"),
+        // mate in 2
+        std::make_tuple("r1bq1rk1/p3bpp1/1p2p2p/2p5/3PN3/2PQPN1P/PPB2nP1/R5K1 w - - 0 19", "e4f6"),
+        std::make_tuple("rn2k1nr/1pp2ppp/p7/8/4N3/3PQ3/PqP3PP/R3KB1R w KQkq - 0 15", "e4f6"),
+        std::make_tuple("8/R3B3/6k1/6pp/6Pn/4P3/PP3P1K/6r1 b - - 2 33", "h4f3"),
+    };
+
+    for (const auto &testCase : testCases)
+    {
+        std::string fen;
+        std::string move;
+        std::tie(fen, move) = testCase;
+
+        engine::Bot bot;
+        bot.setPosition(fen);
+        REQUIRE(bot.chooseMove() == move);
     }
 }

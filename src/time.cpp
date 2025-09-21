1e3421f311e64214f7dbf4049f243c20717bbd88
cosimonegri <cosimogiovanninegri@gmail.com>
2025-09-22 01:05:37 +0200

    Improve calculation of time to think

--- src/time.cpp
+++ src/time.cpp
@@ -1,3 +1,4 @@
+#include <limits>
 #include "time.hpp"
 
 namespace engine
@@ -5,20 +6,31 @@ namespace engine
     uint64_t calcThinkTimeMs(ThinkInfo info, Color side)
     {
         if (info.flags & (F_INFINITE | F_DEPTH | F_NODES))
-            return MAX_THINK_TIME_MS;
+            return INF_THINK_TIME_MS;
 
         if (info.flags & F_MOVETIME)
             return info.moveTime;
 
         uint64_t thinkTimeMs = 0;
+        uint64_t maxThinkTimeMs = INF_THINK_TIME_MS;
+
         int movesToGo = info.flags & F_MOVESTOGO
                             ? info.movesToGo + 2
                             : DEFAULT_MOVESTOGO;
 
         if (info.flags & F_TIME)
-            thinkTimeMs += info.time[side] / movesToGo;
+        {
+            uint64_t timeLeftMs = info.time[side] * (1.0 - SAFETY_MARGIN);
+            thinkTimeMs += timeLeftMs / movesToGo;
+            maxThinkTimeMs = timeLeftMs / 2;
+        }
         if (info.flags & F_INC)
-            thinkTimeMs += info.increment[side] * 0.95;
+        {
+            thinkTimeMs += info.increment[side] * INC_MULTIPLIER;
+        }
+
+        thinkTimeMs = std::max(thinkTimeMs, MIN_THINK_TIME_MS);
+        thinkTimeMs = std::min(thinkTimeMs, maxThinkTimeMs);
 
         return thinkTimeMs;
     }
--- src/time.hpp
+++ src/time.hpp
@@ -6,10 +6,12 @@
 
 namespace engine
 {
-    constexpr uint64_t MAX_THINK_TIME_MS = 100000000;
-    constexpr uint64_t STD_THINK_TIME_MS = 100;
+    constexpr uint64_t INF_THINK_TIME_MS = 100000000;
+    constexpr uint64_t MIN_THINK_TIME_MS = 10;
 
     constexpr int DEFAULT_MOVESTOGO = 30;
+    constexpr double SAFETY_MARGIN = 0.05;
+    constexpr double INC_MULTIPLIER = 0.8;
 
     uint64_t calcThinkTimeMs(ThinkInfo info, Color side);
 
